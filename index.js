ef1a872584c336c6aa8f6ad088e2d45ff2c3efe0
na-br-wo <nathan.b.wood@gmail.com>
2023-04-30 20:11:28 -0600

    added functionality for manager id, email, office number

--- index.js
+++ index.js
@@ -12,9 +12,27 @@ function addManager() {
       type: 'input',
       name: 'name',
       message: "Welcome to the Team Webpage Generator!\nPlease enter the team manager's name:"
+    },
+    {
+      type: 'input',
+      name: 'id',
+      message: "Please enter the manager's employee ID:"
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: "Please enter the manager's email address:"
+    },
+    {
+      type: 'number',
+      name: 'officeNum',
+      message: "Please enter the manager's office number:"
     }
   ]).then((answers) => {
     console.log(`Hello, ${answers.name}!`)
+    console.log(`ID: ${answers.id}`)
+    console.log(`Email: ${answers.email}`)
+    console.log(`Office: ${answers.officeNum}`)
   })
 }
 
